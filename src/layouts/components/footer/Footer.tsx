ba8ee7278d66390bfc5839dd932af84b07180972
이현범 <gus4214@gmail.com>
2023-10-18 19:03:31 +0900

    푸터 작업

--- src/layouts/components/footer/Footer.tsx
+++ src/layouts/components/footer/Footer.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 const Footer = () => {
 	return (
-		<div className='h-28 px-96 py-10 bg-slate-700 justify-start items-center inline-flex'>
-			<div className='flex-col justify-start items-start gap-4 inline-flex'>
-				<div className='justify-start items-start gap-4 inline-flex'>
-					<div className='opacity-70 text-center text-white text-xs font-medium leading-3'>개인정보처리방침</div>
-					<div className='opacity-50 text-center text-white text-xs font-normal leading-3'>이용약관</div>
+		<div className='h-[120px] py-10 bg-slate-700 flex w-full items-center justify-center'>
+			<div className='w-[1200px] flex justify-start'>
+				<div className='flex flex-col gap-4'>
+					<div className='justify-start items-start gap-4 flex'>
+						<span className='text-center text-slate-200 text-[13px] font-medium'>개인정보처리방침</span>
+						<span className='text-center text-slate-200 text-[13px] font-normal'>이용약관</span>
+					</div>
+					<span className='opacity-50 text-center text-white text-[13px] font-normal'>@ 2023 Pliper. All Rights Reserved.</span>
 				</div>
-				<div className='opacity-50 text-center text-white text-xs font-normal leading-3'>@ 2023 Pliper. All Rights Reserved.</div>
 			</div>
 		</div>
 	);
