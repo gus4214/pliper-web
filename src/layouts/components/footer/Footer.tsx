2913f054959533504da2f9336e742a8cfc9212ec
이현범 <gus4214@gmail.com>
2023-10-20 17:37:03 +0900

    fix: 푸터 고정

--- src/layouts/components/footer/Footer.tsx
+++ src/layouts/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
 	return (
 		<div className='h-[120px] py-10 bg-slate-700 flex w-full items-center justify-center'>
-			<div className='w-[1200px] flex justify-start'>
+			<div className='w-[1200px] flex justify-start px-4'>
 				<div className='flex flex-col gap-4'>
 					<div className='justify-start items-start gap-4 flex'>
 						<span className='text-center text-slate-200 text-[13px] font-medium'>개인정보처리방침</span>
