2fc7b2394bd489e6c487c3f55875528d1ab558ce
gus4214 <98795239+gus4214@users.noreply.github.com>
2023-11-01 21:16:29 +0900

    style: 프로필 수정화면 퍼블리싱 중

---
+++ src/pages/mypage/edit/index.tsx
@@ -0,0 +1,12 @@
+import GoBackToMyPageButton from '@/src/components/atoms/buttons/GoBackToMyPageButton';
+import React from 'react';
+
+const MyProfileEditPage = () => {
+	return (
+		<div className='w-full flex justify-center'>
+			<GoBackToMyPageButton />
+		</div>
+	);
+};
+
+export default MyProfileEditPage;
