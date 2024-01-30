1effbe06a0ca161361d54553bed914732cb5b3b5
gus4214 <98795239+gus4214@users.noreply.github.com>
2024-01-30 22:12:44 +0900

    fix: ErrorFallback 버튼 수정

--- src/components/atoms/suspense/fallback/ErrorFallback.tsx
+++ src/components/atoms/suspense/fallback/ErrorFallback.tsx
@@ -1,11 +1,12 @@
+import { Button } from 'react-daisyui';
 import { FallbackProps } from 'react-error-boundary';
 
 export function ErrorFallback({ error, resetErrorBoundary }: FallbackProps) {
-	console.log(error);
+	console.log("🚀 ~ file: ErrorFallback.tsx:5 ~ ErrorFallback ~ error:", error)
 	return (
 		<div>
 			<h1>정보를 불러오는데 실패하였습니다</h1>
-			<button onClick={resetErrorBoundary}>다시 시도</button>
+			<Button onClick={resetErrorBoundary}>다시 시도</Button>
 		</div>
 	);
 }
